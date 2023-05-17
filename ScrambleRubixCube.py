cf16d9da5fcc1e5265d1b2666370312178879c53
Qendresa-Potoku <94839915+Qendresa-Potoku@users.noreply.github.com>
2023-05-17 11:31:57 +0200

    Krijimi _i_ScrambleRubixCube.py
    ScrambleRubixCube

--- ScrambleRubixCube.py
+++ ScrambleRubixCube.py
@@ -112,3 +112,91 @@ def RightACW(x):  # action 10
     RightCW(x)
     RightCW(x)
     RightCW(x)
+
+def BackCW(x):  # action 11 Front  back clock wise
+
+    x[12:15, :] = np.fliplr(x[12:15, :].transpose())
+    temp1 = np.array(x[0, 0:3])
+    temp2 = np.array(x[3:6, 0])
+    temp3 = np.array(x[17, 0:3])
+    temp4 = np.array(x[9:12, 2])
+    x[0, 0:3] = temp4
+    x[3:6, 0] = np.fliplr([temp1])[0]
+    x[17, 0:3] = temp2
+    x[9:12, 2] = np.fliplr([temp3])[0]
+
+
+def BackACW(x):  # action 12
+    BackCW(x)
+    BackCW(x)
+    BackCW(x)
+
+
+def PrintCube(x):
+    print("             ", x[0, 0:3])
+    print("             ", x[1, 0:3])
+    print("             ", x[2, 0:3])
+    print(x[3, 0:3], x[6, 0:3], x[9, 0:3], x[12, 0:3])
+    print(x[4, 0:3], x[7, 0:3], x[10, 0:3], x[13, 0:3])
+    print(x[5, 0:3], x[8, 0:3], x[11, 0:3], x[14, 0:3])
+    print("             ", x[15, 0:3])
+    print("             ", x[16, 0:3])
+    print("             ", x[17, 0:3])
+
+
+def make_move(x, move, reverse):
+    # move number
+    # reverse if 0 original move if 1 reverse of input move
+
+    if reverse == 1:
+        if move % 2 == 0:
+            move = move - 1
+        else:
+            move = move + 1
+    if move == 1:
+        FrontCW(x)
+        return "FrontCW"
+
+    if move == 2:
+        FrontACW(x)
+        return "FrontACW"
+
+    if move == 3:
+        UpCW(x)
+        return "UpCW"
+
+    if move == 4:
+        UpACW(x)
+        return "UpACW"
+
+    if move == 5:
+        DownCW(x)
+        return "DownCW"
+
+    if move == 6:
+        DownACW(x)
+        return "DownACW"
+
+    if move == 7:
+        LeftCW(x)
+        return "LeftCW"
+
+    if move == 8:
+        LeftACW(x)
+        return "LeftACW"
+
+    if move == 9:
+        RightCW(x)
+        return "RightCW"
+
+    if move == 10:
+        RightACW(x)
+        return "RightACW"
+
+    if move == 11:
+        BackCW(x)
+        return "BackCW"
+
+    if move == 12:
+        BackACW(x)
+        return "BackACW"
