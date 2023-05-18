0301222fdd732ddb1fc36d09c207403742a8483a
Melos Ymeri <94323012+MelosYmeri@users.noreply.github.com>
2023-05-18 14:51:10 +0200

    Update RubixcubeSolution.py

--- RubixcubeSolution.py
+++ RubixcubeSolution.py
@@ -73,3 +73,49 @@ def contains2(child, frontier):
     return False
 
 
+def ida(start):
+    start.h = corner_edge_sum_max(start.cube)
+    cost_limit = start.h
+    nodes = 0
+    frontier = list()
+    branching_factors = list()
+
+    while True:
+        minimum = None
+        frontier.append(start)
+
+        while len(frontier) != 0:
+            curr = frontier.pop()
+
+            if goal_reached(curr):
+                print('Goal Height:', curr.g)
+                print('Branching Factor:', sum(branching_factors)/len(branching_factors))
+                # while curr is not None:
+                #    if curr.move is not None:
+                #        print(curr.move)
+                #    curr = curr.parent
+                print("Nodes Generated:", nodes)
+                return
+
+            b = 0
+            nodes = nodes + 12
+            for i in range(12):
+                new = State()
+                new.cube = np.array(curr.cube)
+                new.g = curr.g + 1
+                new.parent = curr
+                new.move = make_move(new.cube, i + 1, 0)
+                new.h = corner_edge_sum_max(new.cube)
+
+                if new.g + new.h > cost_limit:
+                    if minimum is None or new.g + new.h < minimum:
+                        minimum = new.g + new.h
+                    continue
+                if curr.parent is not None and (contains1(new.cube, curr) or contains2(new.cube, frontier)):
+                    continue
+                frontier.append(new)
+                b = b + 1
+            if b != 0:
+                branching_factors.append(b)
+
+        cost_limit = minimum
